560f55b2429cc7801f54c5bfd8bb2cb8b665259d
Sean Chambers <schambers80@gmail.com>
2025-08-20 08:53:37 -0500

    greatly simplify renaming script that works properly

--- scripts/init.sh
+++ scripts/init.sh
@@ -1,33 +1,21 @@
 #!/bin/bash
-# init.sh
-# Usage: ./init.sh NewPluginName
 
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <NewPluginName>"
-    exit 1
-fi
+[ $# -eq 0 ] && { echo "Usage: $0 <TARGET_NAME>"; exit 1; }
 
-TARGET_NAME="$1"
-TEMPLATE_NAME="AudioPlugin"
-TEMPLATE_FULL_NAME="AudioPluginTemplate"
+TARGET="$1"
 
-for target in "$OLD_FULL_NAME" "$OLD_NAME"; do
-    # Directory renaming
-    find . -path ./.git -prune -o -depth -type d -name "*$target*" -print | while read -r dir; do
-        new_dir=$(echo "$dir" | sed "s/$target/$TARGET_NAME/g")
-        mv "$dir" "$new_dir"
-    done
-
-    # File renaming
-    find . -path ./.git -prune -o -type f -name "*$target*" -print | while read -r file; do
-        new_file=$(echo "$file" | sed "s/$target/$TARGET_NAME/g")
-        mv "$file" "$new_file"
-    done
+# Rename content in text files
+find . -type f -not -path "./.git/*" -exec grep -l "AudioPlugin" {} + 2>/dev/null | \
+while read file; do
+    sed -i.bak "s/AudioPluginTemplate/$TARGET/g; s/AudioPlugin/$TARGET/g" "$file"
+    rm "$file.bak"
 done
 
-# Replace inside files
-export LC_CTYPE=C
-grep -rIl "$TEMPLATE_NAME" . | xargs sed -i '' "s/$TEMPLATE_NAME/$TARGET_NAME/g"
-grep -rIl "$TEMPLATE_FULL_NAME" . | xargs sed -i '' "s/$TEMPLATE_FULL_NAME/$TARGET_NAME/g"
+# Rename files and directories (deepest first)
+find . -depth -not -path "./.git/*" \( -name "*AudioPlugin*" -o -name "*AudioPluginTemplate*" \) | \
+while read path; do
+    new_path=$(echo "$path" | sed "s/AudioPluginTemplate/$TARGET/g; s/AudioPlugin/$TARGET/g")
+    [ "$path" != "$new_path" ] && mv "$path" "$new_path"
+done
 
-echo "Completed renaming from $TEMPLATE_NAME to $TARGET_NAME."
+echo "Renamed AudioPlugin* to $TARGET"
